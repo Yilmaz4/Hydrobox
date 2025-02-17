3c3a6eecda262b967d8e334e8cef56088bfc539d
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2025-02-17 22:26:11 +0300

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-int main() {
+GLFWwindow* window;
+
+class Hydrobox {
+public:
     GLFWwindow* window;
 
-    if (!glfwInit()) return -1;
+    Hydrobox() {
+        GLFWwindow* window;
 
-    window = glfwCreateWindow(640, 480, "GLFW Demo", NULL, NULL);
-    if (!window) {
+        if (!glfwInit()) throw std::exception();
+
+        window = glfwCreateWindow(640, 480, "GLFW Demo", NULL, NULL);
+        if (!window) {
+            glfwTerminate();
+            throw std::exception();
+        }
+
+        glfwMakeContextCurrent(window);
+
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glClearColor(1.0f, 0.0f, 0.0f, 1.f);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
         glfwTerminate();
+    }
+};
+
+int main() {
+    try {
+        Hydrobox app;
+    } catch (std::exception e) {
         return -1;
     }
-
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwTerminate();
     return 0;
 }
